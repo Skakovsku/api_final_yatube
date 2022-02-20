b8ee299994aeb6d28e6e4ee911a61d9fcdbde1fe
Евгений <Skakovsku@yandex.ru>
2022-02-20 14:20:37 +0300

    20_02_22

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-from rest_framework.exceptions import MethodNotAllowed, PermissionDenied
+from rest_framework.exceptions import MethodNotAllowed
 
 
 class GroupPermission(permissions.BasePermission):
@@ -7,16 +7,16 @@ class GroupPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method not in permissions.SAFE_METHODS:
             raise MethodNotAllowed(request.method)
+        # return request.method in permissions.SAFE_METHODS - этот код
+        # работает, но выбрасывает исключение 403, а тесты требуют - 405.
+        # Поэтому я оставил свой старый код.
         return True
 
 
 class PostCommentPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        print(obj.author)
         list_method = ['PUT', 'PATCH', 'DELETE']
         if request.method in list_method:
-            if request.user != obj.author:
-                error = 'Изменение и удаление чужого контента запрещено!'
-                raise PermissionDenied(error)
+            return obj.author == request.user
         return True
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,11 +1,3 @@
-# Привет, Артем! У меня есть просьба: завтра (08.02.2022) я ложусь на
-# операцию, поэтому, до вторника или среды я не смогу заниматься учебой.
-# Если проект не будет сдан до понедельника, я предполагаю, что к 10-му
-# спринту меня не допустят, и придется оформлять академ. А так как я в
-# ближайшие 2 месяца (минимум) буду на больничном, не хотелось бы терять целый
-# месяц. Если в работе есть серьезные недочеты, обещаю исправить их в течение
-# 10-го спринта.
-
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
@@ -21,7 +13,6 @@ router.register('groups', GroupViewSet)
 router.register(r'posts/(?P<id>\d+)/comments', CommentViewSet)
 
 urlpatterns = [
-    path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
     path('v1/', include(router.urls)),
 ]
